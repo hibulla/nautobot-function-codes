56d05c4bc705972bb0da4e6e0c4f5c116a8106b8
Bartosz <gradys.bartosz@gmail.com>
2026-07-05 18:57:28 +0200

    added icon

--- nautobot_function_codes/navigation.py
+++ nautobot_function_codes/navigation.py
@@ -1,45 +1,75 @@
-"""Menu items."""
+"""Navigation menu items for the Function Codes plugin."""
 
-from nautobot.apps.ui import NavMenuAddButton, NavMenuGroup, NavMenuItem, NavMenuTab
+from django.templatetags.static import static
+from nautobot.apps.ui import (
+    NavMenuAddButton,
+    NavMenuGroup,
+    NavMenuItem,
+    NavMenuTab,
+    NavigationWeightChoices,
+)
+
+# Material Design Icons equivalent: mdi mdi-tag-multiple
+FUNCTION_CODES_TAB_ICON = static("nautobot_function_codes/icons/tag-multiple.svg")
+
+VIEW_FUNCTION_CODE = "nautobot_function_codes.view_functioncode"
+VIEW_DEVICE_ASSIGNMENT = "nautobot_function_codes.view_devicefunctioncodeassignment"
+ADD_FUNCTION_CODE = "nautobot_function_codes.add_functioncode"
+ADD_DEVICE_ASSIGNMENT = "nautobot_function_codes.add_devicefunctioncodeassignment"
+CHANGE_FUNCTION_CODE = "nautobot_function_codes.change_functioncode"
 
 function_code_items = (
     NavMenuItem(
         link="plugins:nautobot_function_codes:functioncode_list",
         name="Function Codes",
-        permissions=["nautobot_function_codes.view_functioncode"],
+        weight=100,
+        permissions=[VIEW_FUNCTION_CODE],
         buttons=(
             NavMenuAddButton(
                 link="plugins:nautobot_function_codes:functioncode_add",
-                permissions=["nautobot_function_codes.add_functioncode"],
+                permissions=[ADD_FUNCTION_CODE],
             ),
         ),
     ),
     NavMenuItem(
         link="plugins:nautobot_function_codes:devicefunctioncodeassignment_list",
         name="Device Assignments",
-        permissions=["nautobot_function_codes.view_functioncode"],
+        weight=200,
+        permissions=[VIEW_DEVICE_ASSIGNMENT],
         buttons=(
             NavMenuAddButton(
                 link="plugins:nautobot_function_codes:devicefunctioncodeassignment_add",
-                permissions=["nautobot_function_codes.change_functioncode"],
+                permissions=[ADD_DEVICE_ASSIGNMENT],
             ),
         ),
     ),
     NavMenuItem(
         link="plugins:nautobot_function_codes:coverage_dashboard",
         name="Coverage",
-        permissions=["nautobot_function_codes.view_functioncode"],
+        weight=300,
+        permissions=[VIEW_FUNCTION_CODE],
     ),
     NavMenuItem(
         link="plugins:nautobot_function_codes:import_assignments",
         name="Import Assignments",
-        permissions=["nautobot_function_codes.change_functioncode"],
+        weight=400,
+        permissions=[CHANGE_FUNCTION_CODE],
     ),
 )
 
 menu_items = (
     NavMenuTab(
         name="Function Codes",
-        groups=(NavMenuGroup(name="Function Codes", items=function_code_items),),
+        weight=NavigationWeightChoices.ORGANIZATION + 10,
+        icon=FUNCTION_CODES_TAB_ICON,
+        permissions=[VIEW_FUNCTION_CODE],
+        groups=(
+            NavMenuGroup(
+                name="Function Codes",
+                weight=100,
+                permissions=[VIEW_FUNCTION_CODE],
+                items=function_code_items,
+            ),
+        ),
     ),
 )
